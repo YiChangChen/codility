834778cdc7434c4ffcb00585ed9f35af8bee10c4
YiChangChen <34483067+YiChangChen@users.noreply.github.com>
2018-10-17 23:23:48 +0800

    Add files via upload

--- PassingCars.java
+++ PassingCars.java
@@ -6,5 +6,33 @@ public class PassingCars {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	 public int solution(int[] A) {
+	        // write your code in Java SE 8
+	        //O(n*n)   
+		    int result = 0;
+	        /*for(int i = 0; i<A.length;i++){
+	            for(int j = i;j<A.length;j++){
+	                if(A[i] == 0 && A[j] == 1){
+	                    result++;
+	                }
+	            }
+	        }*/
+	        
+	        // O(n)
+	        int east = 0;
+	        for(int i = 0; i<A.length; i++){
+		       if(A[i] == 0){
+		           east++;
+		       }
+		       else{
+		           result+=east;
+		       }
+		   }
+		   
+		   if(Math.abs(result) > 1000000000){
+		       return -1;
+		   }
+		   return result;
+	    }
 }
